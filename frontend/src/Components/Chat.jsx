70724304fd88a16e49250210b4a7ac74247fcd97
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-03-12 23:53:26 +0530

    Components

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Message from './Message'
+import Input from './Input'
 
 const Chat = () => {
+    const { data } = useContext(ChatContext);
   return (
-    <div>Chat</div>
+    <div className="chat">
+      <div className="chatInfo">
+        <span>{data.user?.displayName}</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Message/>
+      <Input/>
+    </div>
   )
 }
 
