28d76c28bf406b443ede05f9ff39329298e47343
Muskangupta2507 <100120446+Muskangupta2507@users.noreply.github.com>
2024-03-12 23:43:40 +0530

    1stcommit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
 const express = require("express");
+const app=express();
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Chat App")
+})
+
+
+
+
+
+
+
+
+app.listen(3000, ()=> 
+console.log("Server Running on port 3000")
+);
+
